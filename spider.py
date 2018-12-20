90dff9fb836c49626c2d0c1d7f51f430b5a51597
Salman Sakib <salmansrizon2016@gmail.com>
2018-12-20 13:15:49 +0600

    Update Spaider.py
    Added Url parsing for pickaboo

--- spider.py
+++ spider.py
@@ -5,34 +5,38 @@ from scrapy import Request
 class MultipleQuotesSpider(scrapy.Spider):
     name = "multiple-quotes"
     allowed_domains = ["pickaboo.com"]
-    start_urls = ['https://www.pickaboo.com/mobile-tablet.html/']
+    start_urls = [
+        'https://www.pickaboo.com/mobile-tablet.html/']
 
     def parse(self, response):
-        product_title = response.css('h2.product-name.newname>a::text').extract()
+        product_title = response.css(
+            'h2.product-name.newname>a::text').extract()
+
         if response.css('.regular-price'):
             Current_price = response.css('span::attr(content)').extract()
         else:
             return 0
 
+        product_url = response.css(
+            'h2.product-name.newname>a').xpath('@href').extract()
+
         # li_tag = response.xpath("//li[contains(concat(' ', @class, ' '), ' old-price ')]")
-        # if li_tag: 
+        # if li_tag:
         #     old_price = response.css('div.price-box>p.old-price>span.price::text').extract()
         #     pass
         # else:
         #     return None
-             
+        # old_price = response.xpath('//span[starts-with(@id, "old-price-")]/text()').extract()
 
-
-
-        for item in zip(product_title,Current_price):
+        for item in zip(product_title, Current_price, product_url):
 
             info = {
-                'Product Name' : item[0],
-                'Current Price' : item[1],
-                # 'Old Price': item[2]
+                'Product Name': item[0],
+                'Current Price': item[1],
+                'Product URL:': item[2]
             }
 
             yield info
 
         next_page_url = response.css('a.next::attr(href)').extract_first()
-        yield scrapy.Request(url = next_page_url, callback=self.parse)
+        yield scrapy.Request(url=next_page_url, callback=self.parse)
